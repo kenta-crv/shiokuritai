810f774fb96b56e5cf7e0aa33c90e412fe285dcf
tacohachi <zero0hati8tk8@gmail.com>
2020-09-20 01:22:34 +0900

    メッセージ送信機能

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,21 +1,39 @@
 class MessagesController < ApplicationController
-  before_action :redirect_room_invalid_user, only: :room
-
   def room
-    room = Room.find_by(uri_token: params[:uri_token])
-    @messages = room.messages
-    @member = room.member
-    @user = room.user
+    @room = Room.find_by(uri_token: params[:uri_token])
+    redirect_invalid_user(@room)
+    @messages = @room.messages
+    @member = @room.member
+    @user = @room.user
     # 既読にする
     @read_user = current_user.present? ? current_user : current_member
-    Message.read(room, @read_user)
+    Message.read(@room, @read_user)
+
+    @message = Message.new
+  end
+
+  def create
+    @room =  Room.find_by(params[:id])
+    redirect_invalid_user(@room)
+    @message = Message.new(message_params)
+
+    if @message.save
+      redirect_to room_messages_path(uri_token: @room.uri_token), alert: "メッセージを送信しました"
+    else
+      @messages = @room.messages
+      @member = @room.member
+      @user = @room.user
+      render :room
+    end
   end
 
   private
-    def redirect_room_invalid_user
-      room =  Room.find_by(uri_token: params[:uri_token])
-      return render_404 if room.blank? || !room.active?
+    def message_params
+      params.require(:message).permit(:content, :room_id, :is_user)
+    end
 
+    def redirect_invalid_user(room)
+      return render_404 if room.blank? || !room.active?
 
       member = room.member
       user = room.user
