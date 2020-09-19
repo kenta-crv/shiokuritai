3d58fc450e62a9a33a0fac8d0c2f2885bbb241f3
tacohachi <zero0hati8tk8@gmail.com>
2020-09-19 14:39:39 +0900

    letter_openerの導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
+  
   #管理者アカウント
   devise_for :admins, controllers: {
     registrations: 'admins/registrations',
