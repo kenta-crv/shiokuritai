96214ac14ecae1a9efd7b7da4b6c6e2ce7f6058f
kenta-crv <adp@crold-valieat.jp>
2020-09-24 17:44:55 +0900

    fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'shiokuritai.com' }
   config.action_mailer.raise_delivery_errors = true
 
   ActionMailer::Base.delivery_method = :letter_opener_web
